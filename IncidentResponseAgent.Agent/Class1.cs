bbffe91af2937ac7f73b32c75dc666ed3eafdbf4
Aseel-Sh <aseel.sh52@gmail.com>
2026-03-31 20:44:48 -0500

    init commit

---
+++ IncidentResponseAgent.Agent/Class1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace IncidentResponseAgent.Agent;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection AddAgent(this IServiceCollection services)
+	{
+		return services;
+	}
+}
---
+++ IncidentResponseAgent.Api/Program.cs
@@ -0,0 +1,33 @@
+using IncidentResponseAgent.Agent;
+using IncidentResponseAgent.Application;
+using IncidentResponseAgent.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+builder.Services.AddAgent();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapGet("/health", () => Results.Ok(new { status = "Healthy" }))
+    .WithName("Health");
+
+app.MapControllers();
+
+app.Run();
